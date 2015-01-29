de4923343b700bba3182586a5ae3b52416bd107e
Varun Chitre <varun.chitre15@gmail.com>
2015-01-29 06:36:49 -0500

    asoc: fixup headset buttons for taoshan
    Signed-off-by: Varun Chitre <varun.chitre15@gmail.com>

--- sound/soc/msm/msm8930.c
+++ sound/soc/msm/msm8930.c
@@ -876,7 +876,7 @@ end:
 
 static int msm8930_audrx_init(struct snd_soc_pcm_runtime *rtd)
 {
-	int err;
+	int err,ret;
 	struct snd_soc_codec *codec = rtd->codec;
 	struct snd_soc_dapm_context *dapm = &codec->dapm;
 	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
@@ -913,6 +913,14 @@ static int msm8930_audrx_init(struct snd_soc_pcm_runtime *rtd)
 		return err;
 	}
 	codec_clk = clk_get(cpu_dai->dev, "osr_clk");
+    
+    ret = snd_jack_set_key(button_jack.jack,
+			       SND_JACK_BTN_0,
+			       KEY_MEDIA);
+	if (ret) {
+		pr_err("%s: Failed to set code for btn-0\n", __func__);
+		return err;
+	}
 
 	/*
 	 * Switch is present only in 8930 CDP and SGLTE
